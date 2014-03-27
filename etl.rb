c37e2436dfe3580ab194c035b5aed9c10d1f4c80
liza <liza@localhost>
2014-03-27 17:58:40 +0600

    ??? ??????? ???????? ????????? ????????? ???????????? id ???????.

--- etl.rb
+++ etl.rb
@@ -4,9 +4,25 @@ require 'mysql'
 # require 'sqlite3'
 require 'thread'
 
+class Company
+	def initialize(storage, affiliate_id, last_s, last_p, last_sp)
+		@storage = storage
+		@affiliate_id = affiliate_id
 
-class AffiliateOne 
-	def initialize()
+		@last_s = last_s
+		@last_p = last_p
+		@last_sp = last_sp
+	end
+
+	def load()
+		storage.query('INSERT INTO updates (time, affiliate_id, s, p, sp) \
+						VALUES (UNIX_TIMESTAMP(), #{@affiliate_id}, #{@last_s}. #{@last_p}, #{@last_sp})')
+	end
+end
+
+class AffiliateOne < Company
+	def initialize(storage, affiliate_id, last_s, last_p, last_sp)
+		super(storage, affiliate_id, last_s, last_p, last_sp)
 		puts 'Company 1 initialized'
 	end
 
@@ -15,8 +31,9 @@ class AffiliateOne
 	end
 end
 
-class AffiliateTwo
-	def initialize()
+class AffiliateTwo < Company
+	def initialize(storage, affiliate_id, last_s, last_p, last_sp)
+		super(storage, affiliate_id, last_s, last_p, last_sp)
 		puts 'Company 2 initialized'
 	end
 
@@ -28,29 +45,30 @@ end
 begin
 	puts 'Start ETL process'
 
+	databases = [
+		AffiliateOne, 
+		AffiliateTwo
+	]
+
+	last_ids = Array.new(databases.count) {{'last_sid' => 0, 'last_pid' => 0, 'last_spid' => 0}}
+
 	storage = Mysql.new 'localhost', 'root', 'finncrisporiginal', 'warehouse'
+
 	res = storage.query('SELECT * FROM updates GROUP BY affiliate_id HAVING time = MAX(time)')
-
-	databases = [AffiliateOne, AffiliateTwo]
-
-	last_ids = Array.new(databases.count) {[{'last_sid' => 0, 'last_pid' => 0, 'last_spid' => 0}]}
-
 	res.num_rows do |i|
 		row = res.fetch_row
-		if (affiliate_id = row['affiliate_id'].to_i > databases.count)
+		if (affiliate_id = row['affiliate_id'] > databases.count)
 			puts "Too big 'affiliate_id' value (#{affiliate_id}) in 'updates' table"
 			next
 		end
 
-		last_ids[i] = [{'last_sid' => row['s'], 'last_pid' => row['p'], 'last_spid' => row['sp']}]
+		last_ids[i] = {'last_sid' => row['s'], 'last_pid' => row['p'], 'last_spid' => row['sp']}
 	end
-
-	puts last_ids
 
 	threads = []
 	databases.each_with_index {|database, i|
 		threads << Thread.new() {
-			database.new().extract()
+			database.new(storage, i, last_ids[i]['last_sid'], last_ids[i]['last_pid'], last_ids[i]['last_spid']).extract()
 		}
 	}
 	threads.each {|t| t.join}
