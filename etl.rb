a0f2faf1d8deaad3ecbc3d3f47912c34fdb96a93
liza <liza@localhost>
2014-03-30 19:35:21 +0600

    ??????????, ?????????????? ? ???????? ?????? ?? ????? ???? ?????? ? ?????????.

--- etl.rb
+++ etl.rb
@@ -3,6 +3,7 @@
 require 'mysql'
 # require 'sqlite3'
 require 'thread'
+require 'bigdecimal'
 
 class Company
 	def initialize(storage, affiliate_id, last_s, last_p, last_sp)
@@ -17,12 +18,12 @@ class Company
 		@max_p = 0
 		@max_sp = 0
 
-		self.create_connection
-
 		puts "Company #{@affiliate_id} initialized"
 	end
 
 	def extract()
+		self.create_connection
+
 		self.extract_max_ids
 
 		self.extract_suppliers
@@ -35,17 +36,112 @@ class Company
 	end
 
 	def transform()
+		self.create_suppliers
+		self.create_parts
+		self.create_shipments
+
 		puts "Data of affiliate #{@affiliate_id} transformed"
 	end
 
 	def load()
-		return if @suppliers == nil or @addresses == nil or @parts == nil or @shipments == nil
-		return if @suppliers.count + @addresses.count + @parts.count + @shipments.count == 0
-
-		@storage.query("INSERT INTO updates (time, affiliate_id, s, p, sp) \
-				VALUES (UNIX_TIMESTAMP(), #{@affiliate_id}, #{@max_s}, #{@max_p}, #{@max_sp})")
+		return if @shipments_query == nil
+		puts @shipments_query
+		@storage.query(@shipments_query)
+		@storage.query("INSERT INTO updates (time, affiliate_id, s, p, sp) VALUES (UNIX_TIMESTAMP(), #{@affiliate_id}, #{@max_s}, #{@max_p}, #{@max_sp})")
 
 		puts "Values for #{@affiliate_id} affiliate added to warehouse: suppliers (#{@last_s}, #{@max_s}), parts (#{@last_p}, #{@max_p}), relations (#{@last_sp}, #{@max_sp})"
+	end
+
+	###
+	### Transform methods
+	###
+	protected
+	def create_suppliers()
+		return if @suppliers.empty?
+
+		suppliers_values = String.new
+		@suppliers.each do |supplier|
+			suppliers_values << ',' if not suppliers_values.empty?
+			suppliers_values << '("'	<< supplier[:name] << '","' << supplier[:address] << '","' << supplier[:city] << '")'
+		end
+
+		@storage.query ("INSERT IGNORE INTO suppliers(name, address, city) VALUES" + suppliers_values)
+	end
+
+	protected	
+	def create_parts()
+		return if @parts.empty?
+
+		parts_values = String.new
+		@parts.each do |part|
+			parts_values << ',' if not parts_values.empty?
+			parts_values << '("'	<< part[:name] << '",' << part[:htp] << ',' << part[:weight] << ')'
+		end
+
+		@storage.query "INSERT IGNORE INTO parts(name, HTP, weight) VALUES" + parts_values
+	end
+
+	protected	
+	def create_shipments()
+		return if @shipments.empty?
+
+		suppliers_values = String.new
+		parts_values = String.new
+		@shipments.each do |shipment|
+			suppliers_values << ',' if not suppliers_values.empty?
+			suppliers_values << '("'	<< shipment[:supplier] << '","' << shipment[:address] << '","' << shipment[:city] << '")'
+
+			parts_values << ',' if not parts_values.empty?
+			parts_values << '("' << shipment[:part] << '",' << shipment[:part_weight] << ')'
+
+		end
+
+		supplier_ids = @storage.query('SELECT id, name, address, city FROM suppliers WHERE (name, address, city) IN (' + suppliers_values + ')')
+
+		sids = Hash.new
+		supplier_ids.each_hash do |supplier|
+			city = supplier['city']
+			name = supplier['name']
+			address = supplier['address']
+			id = supplier['id']
+
+			sids[city] = Hash.new unless sids.key? city 
+			by_city = sids[city]
+
+			by_city[name] = Hash.new unless by_city.key? name
+			by_name = by_city[name]
+
+			by_name[address] = id
+		end
+
+		parts_ids = @storage.query('SELECT id, name, CAST(weight AS DECIMAL(8,4)) as weight FROM parts WHERE (name, CAST(weight AS DECIMAL(8,4))) IN (' + parts_values + ')')
+
+		pids = Hash.new
+		parts_ids.each_hash do |part|
+			name = part["name"]
+			weight = part["weight"]
+			id = part["id"]
+
+			pids[name] = Hash.new if not pids.key? name
+			by_name = pids[name]
+
+			by_name[weight] = id
+		end
+
+		@shipments.each do |shipment|
+			shipment[:supplier_id] = sids[shipment[:city]][shipment[:supplier]][shipment[:address]]
+			shipment[:part_id] = pids[shipment[:part]][shipment[:part_weight]]
+		end
+
+		shipments_values = String.new
+		@shipments.each do |shipment|
+			shipments_values << ',' if not shipments_values.empty?
+			shipments_values << '('	<< shipment[:supplier_id].to_s << ',' << shipment[:part_id] << ',' \
+								<< shipment[:qty] << ',' << shipment[:price] << ',' << shipment[:weight] << ',' \
+								<< shipment[:order_date] << ',' << shipment[:period] << ',' << shipment[:ship_date] << ')'
+		end
+
+		@shipments_query = "INSERT INTO shipments(sid, pid, qty, price, weight, order_date, period, ship_date) VALUES" + shipments_values
 	end
 end
 
@@ -57,7 +153,9 @@ class AffiliateOne < Company
 	protected
 	def create_connection()
 		begin
-			@con = Mysql.new 'localhost', 'root', 'finncrisporiginal', 'company'			
+			@con = Mysql.init
+			@con.options(Mysql::SET_CHARSET_NAME, 'utf8')
+			@con.real_connect('localhost', 'root', 'finncrisporiginal', 'company')
 		rescue Mysql::Error => e
 			puts e.errno
 			puts e.error
@@ -69,6 +167,10 @@ class AffiliateOne < Company
 	def close_connection()
 		@con.close if @con
 	end
+
+	###
+	### Extraction methods
+	###
 
 	protected
 	def extract_max_ids()
@@ -93,14 +195,14 @@ class AffiliateOne < Company
 	def extract_suppliers()
 		begin
 			res = @con.query("SELECT * FROM S \
-					WHERE SID BETWEEN #{@last_s} AND #{@max_s} AND \
+					WHERE SID BETWEEN #{@last_s} + 1 AND #{@max_s} AND \
 					SName IS NOT NULL AND \
 					SCity IS NOT NULL AND \
 					Address IS NOT NULL")
 
-			@suppliers = Array.new(res.num_rows)
+			@suppliers = Array.new
 			res.each_hash do |row|
-				@suppliers << {:supplier => row["SName"], :city => row["SCity"], :address => row["Address"]}
+				@suppliers << {:name => row["SName"], :city => row["SCity"], :address => row["Address"]}
 			end
 		rescue Mysql::Error => e
 			puts e.errno
@@ -112,12 +214,12 @@ class AffiliateOne < Company
 	protected
 	def extract_parts()
 		begin
-			res = @con.query("SELECT * FROM P \
-					WHERE PID BETWEEN #{@last_p} AND #{@max_p} AND
+			res = @con.query("SELECT PName, HTP, CAST(Weight AS DECIMAL(8,4)) AS Weight FROM P \
+					WHERE PID BETWEEN #{@last_p} + 1 AND #{@max_p} AND
 					PName IS NOT NULL AND \
 					Weight > 0")
 
-			@parts = Array.new(res.num_rows)
+			@parts = Array.new
 			res.each_hash do |row|
 				@parts << {:name => row["PName"], :htp => row["HTP"], :weight => row["Weight"]}
 			end
@@ -133,18 +235,21 @@ class AffiliateOne < Company
 		begin
 			res = @con.query("SELECT \
 					S.SName AS Supplier, \
+					S.Address AS Address, \
+					S.SCity AS City, \
 					P.PName AS Part, \
+					CAST(P.Weight AS DECIMAL(8,4)) AS PartWeight, \
 					SP.Qty AS Quantity, \
-					SP.OrderDate AS OrderDate, \
+					UNIX_TIMESTAMP(SP.OrderDate) AS OrderDate, \
 					SP.Period AS Period, \
-					SP.ShipDate AS ShipDate, \
+					UNIX_TIMESTAMP(SP.ShipDate) AS ShipDate, \
 					SP.Price AS Price, \
 					P.Weight * SP.Qty AS SPWeight \
 				FROM SP \
 				INNER JOIN S ON S.SID = SP.SID \
 				INNER JOIN P ON P.PID = SP.PID \
 				WHERE \
-					SP.SPID BETWEEN #{@last_sp} AND #{@max_sp} AND \
+					SP.SPID BETWEEN #{@last_sp} + 1 AND #{@max_sp} AND \
 					S.SName IS NOT NULL AND \
 					S.SCity IS NOT NULL AND \
 					S.Address IS NOT NULL AND \
@@ -157,11 +262,12 @@ class AffiliateOne < Company
 					SP.OrderDate <= SP.ShipDate \
 				HAVING SPWeight BETWEEN 0 AND 1500")
 
-			@shipments = Array.new(res.num_rows)
+			@shipments = Array.new
 			res.each_hash do |row|
-				@shipments << {:supplier => row["Supplier"], :part => row["Part"], :qty => row["Quantity"], 
+				@shipments << {:supplier => row["Supplier"], :city => row["City"], :address => row["Address"], :supplier_id => 0,
+								:part => row["Part"], :part_weight => row["PartWeight"], :qty => row["Quantity"], :part_id => 0,
 								:order_date => row["OrderDate"], :ship_date => row["ShipDate"], :period => row["Period"], 
-								:weight => row["Weight"], :price => row["Price"]}
+								:weight => row["SPWeight"], :price => row["Price"]}
 			end
 		rescue Mysql::Error => e
 			puts e.errno
@@ -184,6 +290,10 @@ class AffiliateTwo < Company
 	def close_connection()
 	end
 
+	###
+	### Extraction methods
+	###
+
 	protected
 	def extract_max_ids()
 	end
@@ -205,13 +315,14 @@ begin
 	puts 'Start ETL process'
 
 	databases = [
-		AffiliateOne, 
-		AffiliateTwo
+		AffiliateOne
 	]
 
 	last_ids = Array.new(databases.count) {{:last_sid => 1, :last_pid => 1, :last_spid => 1}}
 
-	storage = Mysql.new 'localhost', 'root', 'finncrisporiginal', 'warehouse'
+	storage = Mysql.init
+	storage.options(Mysql::SET_CHARSET_NAME, 'utf8')
+	storage.real_connect('localhost', 'root', 'finncrisporiginal', 'warehouse')
 
 	res = storage.query("SELECT u.affiliate_id AS affiliate_id, u.s AS s, u.p AS p, u.sp AS sp
 						FROM updates u
